21c7bc0a9e52acc626eef1de898b4bbabf1fd0c8
joshpaz0 <46551500+joshpaz0@users.noreply.github.com>
2019-01-31 09:57:27 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/*
+ * GET home page.
+ */
+
+exports.view = function(req, res){
+  res.render('index');
+};
+
+exports.view = function(req, res){
+  res.render('index', {
+    'name': 'Waiting in Line',
+    'image': 'lorempixel.people.1.jpeg',
+    'id': 'project1'
+  });
+};
